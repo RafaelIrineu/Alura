602d8368024dcfbb4326cd093ee92808d222811c
RafaelIrineu <rafaelirineu01@gmail.com>
2021-04-15 12:51:44 -0300

    Refinando o teste do adapter, alterando as chamadas dos metodos

--- TESTES-MOCK/Leilao/app/src/test/java/br/com/alura/leilao/ui/activity/ListaLeilaoActivityTest.java
+++ TESTES-MOCK/Leilao/app/src/test/java/br/com/alura/leilao/ui/activity/ListaLeilaoActivityTest.java
@@ -1,20 +1,38 @@
 package br.com.alura.leilao.ui.activity;
 
+import android.content.Context;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import br.com.alura.leilao.ui.recyclerview.adapter.ListaLeilaoAdapter;
+
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ListaLeilaoActivityTest {
+
+    @Mock
+    private Context context;
+    @Spy
+    private ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(context);
 
     @Test
     public void deve_AtualizarListaDeLeiloes_QuandoBuscarLeiloesDaApi() throws InterruptedException {
         ListaLeilaoActivity activity = new ListaLeilaoActivity();
 
+        Mockito.doNothing().when(adapter).atualizaLista();
+
+        activity.buscaLeiloes(adapter);
         //colocar um delay devido a chamada assincrona, para dar tempo de retornar valor
-        activity.configuraAdapter();
-        activity.buscaLeiloes();
         Thread.sleep(2000);
-        int quantidadeDeLeiloesDevolvida = activity.getAdapter().getItemCount();
+        int quantidadeDeLeiloesDevolvida = adapter.getItemCount();
 
         assertThat(quantidadeDeLeiloesDevolvida,is(3));
     }
