364d80a033f86e4c684efd0845cbf0dc4958b3b9
RafaelIrineu <rafaelirineu01@gmail.com>
2021-04-18 15:27:42 -0300

    Criando mais um teste

--- TESTES-MOCK/Leilao/app/src/test/java/br/com/alura/leilao/api/EnviadorDeLanceTest.java
+++ TESTES-MOCK/Leilao/app/src/test/java/br/com/alura/leilao/api/EnviadorDeLanceTest.java
@@ -7,14 +7,19 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.alura.leilao.api.retrofit.client.RespostaListener;
 import br.com.alura.leilao.exception.LanceMenorQueUltimoLanceException;
+import br.com.alura.leilao.exception.LanceSeguidoDoMesmoUsuarioException;
 import br.com.alura.leilao.exception.UsuarioJaDeuCincoLancesException;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
 import br.com.alura.leilao.ui.dialog.AvisoDialogManager;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -30,7 +35,7 @@ public class EnviadorDeLanceTest {
     private Leilao leilao;
 
     @Test
-    public void deve_MostrarMensagemDeFalha_QuandoLanceForMenorQueUltimoLance(){
+    public void deve_MostrarMensagemDeFalha_QuandoLanceForMenorQueUltimoLance() {
         EnviadorDeLance enviador = new EnviadorDeLance(
                 client,
                 listener,
@@ -38,25 +43,42 @@ public class EnviadorDeLanceTest {
         );
 
         doThrow(LanceMenorQueUltimoLanceException.class)
-                .when(leilao).propoe(ArgumentMatchers.any(Lance.class));
+                .when(leilao).propoe(any(Lance.class));
 
-        enviador.envia(leilao, new Lance(new Usuario("Mari"),50.0));
+        enviador.envia(leilao, new Lance(new Usuario("Mari"), 50.0));
 
         verify(manager).mostraAvisoLanceMenorQueUltimoLance();
+        verify(client, never()).propoe(any(Lance.class), anyLong(), any(RespostaListener.class));
     }
 
     @Test
-    public void deve_MostrarMensagemDeFalha_QuandoUsuarioComCincoLancesEnviarLance(){
+    public void deve_MostrarMensagemDeFalha_QuandoUsuarioComCincoLancesEnviarLance() {
         EnviadorDeLance enviador = new EnviadorDeLance(
                 client,
                 listener,
                 manager
         );
         doThrow(UsuarioJaDeuCincoLancesException.class)
-                .when(leilao).propoe(ArgumentMatchers.any(Lance.class));
+                .when(leilao).propoe(any(Lance.class));
 
-        enviador.envia(leilao, new Lance(new Usuario("Mari"),200.0));
+        enviador.envia(leilao, new Lance(new Usuario("Mari"), 200.0));
 
         verify(manager).mostraAvisoUsuarioJaDeuCincoLances();
+    }
+
+    @Test
+    public void deve_MostrarMensagemDeFalha_QuandoOMesmoUsuarioDerDoisLancesSeguidos() {
+        EnviadorDeLance enviador = new EnviadorDeLance(
+                client,
+                listener,
+                manager
+        );
+        doThrow(LanceSeguidoDoMesmoUsuarioException.class)
+                .when(leilao).propoe(any(Lance.class));
+
+        enviador.envia(leilao, new Lance(new Usuario("Mari"), 200.0));
+
+        verify(manager).mostraAvisoLanceSeguidoDoMesmoUsuario();
+        verify(client, never()).propoe(any(Lance.class), anyLong(), any(RespostaListener.class));
     }
 }
